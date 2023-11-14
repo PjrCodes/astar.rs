1bcc8d271e84c125f838204a9c13db273b6db480
Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>
2023-11-14 12:52:42 +0530

    add todos

---
+++ bench/main.rs
@@ -0,0 +1,15 @@
+
+// TODOs:
+
+
+// add a new button to the UI that opens a modal to set the benchmark parameters. there is a button that lets u run the benchmark and closes the checkbox
+// the benchmark should update the UI at every iteration with the graph
+// folder choosing option -- maybe just a text box
+// you can run either the pre-generated benchmarks using the random seed or give your own folder
+// it should update the UI and tie in with Nikhil's Import/Export
+
+
+fn main() {
+    // read 
+}
+// build a map that's solvable
